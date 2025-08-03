01b3fb3968d4b36d500c67c389e334b6b196b47c
Hardik-Soni12 <imailhr1k@gmail.com>
2025-08-03 19:55:43 +0530

    My to-do app

---
+++ To-Do-App/app/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email(granular_message=True)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=25)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='passwords must match')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
